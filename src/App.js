5163f82c9772b246e79ca3de69cd5103e3a4d444
Matiur <matiurrahman5559@gmail.com>
2022-06-13 20:03:01 +0600

    user sign in/out functionality with firebase and OAuth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
-import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -15,13 +18,16 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
-          {/* <Route path='/sign-up' element={<SignUp />} /> */}
-          {/* <Route path='/sign-in' element={<Profile />} /> */}
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
+      <ToastContainer />
     </>
   );
 }
